136133f28ea35b466ebdbfb7cfd1dd90b28f3f45
Waseel Sharar <113261290+WaseelSharar@users.noreply.github.com>
2022-09-22 20:12:06 +0200

    Day 4

---
+++ 4-variables.py
@@ -0,0 +1,10 @@
+year = 1
+
+
+print('one year is equal to : ', year , 'year.')
+
+print('one year is equal to : ', year*12 , 'months.')
+print('one year is equal to : ', year*365 , 'days.')
+print('one year is equal to : ', year*365*24 , 'hours.')
+print('one year is equal to : ', year*365*24*60 , 'minutes.')
+print('one year is equal to : ', year*365*24*60*60 , 'seconds.')
